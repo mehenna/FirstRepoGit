0843139bf7a0a1db99478e77a4d6551ff8fe56e5
mehen <mehen@192.168.1.14>
2016-05-22 12:23:45 +0200

    Initial commit

---
+++ localTestProjetDemoOngit/demo/src/main/java/com/example/Testconguration.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Testconguration {
+	
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public Testconguration(String test) {
+		super();
+		this.test = test;
+	}
+
+	public Testconguration() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
